a4155de18bc8081c654da053523244764de638b2
luiscadari <luiscadari@icloud.com>
2025-04-04 17:25:38 -0300

    :tada: First commit!

---
+++ src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+    nome := "Douglas"
+    versao := 1.1
+    fmt.Println("Olá, sr.", nome)
+    fmt.Println("Este programa está na versão", versao)
+
+    fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
+
+    var comando int
+    fmt.Scan(&comando)
+    fmt.Println("O comando escolhido foi", comando)
+
+    if comando == 1 {
+
+    } else if comando == 2 {
+
+    } else if comando == 0 {
+
+    }
+}
