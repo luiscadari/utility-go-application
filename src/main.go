0fd34d641e4bd4277ddcb58b9665c03748f5ecab
luiscadari <luiscadari@icloud.com>
2025-04-04 17:50:43 -0300

    :sparkles: feat: monitoramento web

--- src/main.go
+++ src/main.go
@@ -1,26 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-    nome := "Douglas"
-    versao := 1.1
-    fmt.Println("Olá, sr.", nome)
-    fmt.Println("Este programa está na versão", versao)
 
+    introduction()
+    fmt.Println("Escolha uma das opções acima:")
+    command := inputCommand();
+    switch command {
+    case 0: {
+        fmt.Println("Saindo do programa...")
+        os.Exit(0)
+    }
+    case 1: {
+        fmt.Println("Iniciando o monitoramento...")
+        monitore()
+    }
+    case 2: {
+
+    }
+    default: {
+        fmt.Println("Comando inválido!")   
+        os.Exit(-1)
+    }
+    }
+}
+
+func monitore(){
+    var url string
+    fmt.Println("Digite a URL que deseja monitorar:")
+    fmt.Scan(&url)
+    resp, err := http.Get(url)
+    fmt.Println(err)
+    fmt.Println("Status Code:", resp.StatusCode);
+}
+
+func introduction() {
+    version := 1.1
+    fmt.Println("Este programa está na versão", version)
     fmt.Println("1- Iniciar Monitoramento")
     fmt.Println("2- Exibir Logs")
     fmt.Println("0- Sair do Programa")
+}
 
-    var comando int
-    fmt.Scan(&comando)
-    fmt.Println("O comando escolhido foi", comando)
-
-    if comando == 1 {
-
-    } else if comando == 2 {
-
-    } else if comando == 0 {
-
-    }
+func inputCommand() int {
+    var command int
+    fmt.Println("Escolha uma das opções acima:")
+    fmt.Scan(&command)
+    return command
 }
