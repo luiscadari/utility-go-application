74c5901ffd10ca3ec8898208607f1c5359eb9f42
luiscadari <luiscadari@icloud.com>
2025-04-16 11:00:17 -0300

    :boom: fix: arrumando get file

--- src/main.go
+++ src/main.go
@@ -78,7 +78,7 @@ func inputCommand() int {
 
 func fileInput()[]string{
     var sites []string
-    arquivo, err := os.Open("sites.txt")
+    arquivo, err := os.ReadFile("sites.txt")
     if err != nil {
         fmt.Println("Ocorreu um erro: ", err)
     }
