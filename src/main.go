a3fe2bd28f29676d962a74f5986d99c9c4565825
luiscadari <luiscadari@icloud.com>
2025-04-16 10:21:10 -0300

    :recycle: refactor: utilizando for range
    utilizando for range para verificar os sites!

--- src/main.go
+++ src/main.go
@@ -33,18 +33,14 @@ func main() {
 }
 
 func monitore(){
-    var url string
-    var sites [4]string
-    sites[0] = "https://www.alura.com.br"
-    sites[1] = "www.google.com"
-    fmt.Println(sites)
-    fmt.Println("Digite a URL que deseja monitorar:")
-    fmt.Scan(&url)
-    resp, _ := http.Get(url)
-    if resp.StatusCode == 200 {
-        fmt.Println("O site está no ar!");
-    }else{
-        fmt.Println("O site está fora do ar!");
+    sites := []string{"https://www.alura.com.br", "https://www.google.com"}
+    for index, site := range sites {
+        resp, _ := http.Get(site)
+        if resp.StatusCode == 200 {
+            fmt.Println(index," - ", "O site: ", site, "está no ar!");
+        }else{
+            fmt.Println("O site está fora do ar!");
+        }
     }
 }
 
